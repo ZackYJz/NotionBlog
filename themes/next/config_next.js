c84589d1853daf02f3915828711d60cc6639de91
YiJun Li <zackyjz@qq.com>
2022-04-15 04:55:15 +0800

    Update config_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -5,8 +5,8 @@ const CONFIG_NEXT = {
 
   NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部、屏幕顶部自动折叠，不固定
 
-  POST_LIST_TYPE: 'page', // ['page','scroll] 文章列表样式:页码分页、单页滚动加载
-  POST_LIST_COVER: false, // 文章列表显示封面图
+  POST_LIST_TYPE: 'scroll', // ['page','scroll] 文章列表样式:页码分页、单页滚动加载
+  POST_LIST_COVER: true, // 文章列表显示封面图
   POST_LIST_PREVIEW: true, // 显示文章预览
   POST_LIST_SUMMARY: false, // 显示用户自定义摘要，有预览时优先只展示预览
 
@@ -32,7 +32,7 @@ const CONFIG_NEXT = {
 
   ARTICLE_SHARE: false, // 文章分享功能
   ARTICLE_RELATE_POSTS: true, // 相关文章推荐
-  ARTICLE_COPYRIGHT: true // 文章版权声明
+  ARTICLE_COPYRIGHT: false // 文章版权声明
 
 }
 
